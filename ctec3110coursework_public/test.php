8c4ebbbc5c7f82d4c900eecac7613385bd2ae453
bioreset <darek.szlag@gmail.com>
2019-12-22 14:04:19 +0000

    Final testing of scraping messages

--- ctec3110coursework_public/test.php
+++ ctec3110coursework_public/test.php
@@ -9,13 +9,24 @@ $key = 1;
 try
 {
 	$soap_client_handle = new SoapClient($wsdl);
-	$soap_call_result = $soap_client_handle->peekMessages($username, $password, 1, "");
-	$xml = htmlspecialchars(reset($soap_call_result), ENT_NOQUOTES);
-	echo $xml;
-	$final = simplexml_load_string($xml);
-	echo $final[0]->sourcemsisdn; 
+	$soap_call_result = $soap_client_handle->peekMessages($username, $password, 25, "");
+	for ($i=0; $i<25; $i++){
+	$xml = $soap_call_result[$i];
+	//$final = simplexml_load_string($xml);
+	echo 'MESSAGE ' . $i . ' <br/>Source - ' . $final[0]->sourcemsisdn . 
+	     '<br/>Dest - ' . $final[0]->destinationmsisdn . 
+	     '<br/>Date - ' . $final[0]->receivedtime . 
+	     '<br/>Type - ' . $final[0]->bearer . 
+	     '<br/>Message: '. $final[0]->message .'<br/><br/>'; 
 
-	
+	}
+
+	$xml = $soap_call_result[0];
+	$final = simplexml_load_string($xml);
+	$themessage = $final[0]->message;
+	echo $themessage .'<br/>';
+
+	var_dump($soap_call_result);
 
 }
 catch (SoapFault $exception)
