5cc293d35a21d72a7869622c5d361cc715fe69e3
Seba18997 <36885010+Seba18997@users.noreply.github.com>
2020-01-01 18:22:03 +0000

    Tiny correction Registration form 0.3 cleaned parameters

--- ctec3110coursework_private/app/routes.php
+++ ctec3110coursework_private/app/routes.php
@@ -4,4 +4,4 @@ require 'routes/displayCircutBoardState.php';
 require 'routes/displayMessages.php';
 require 'routes/login.php';
 require 'routes/register.php';
-require 'routes/registeruser.php;
+require 'routes/registeruser.php';
