720d00ed7e2bbbce0c00f4839b817aa8eefecf5a
Seba18997 <36885010+Seba18997@users.noreply.github.com>
2020-01-13 12:14:43 +0000

    Sessions 0.8

--- ctec3110coursework_private/app/routes/login.php
+++ ctec3110coursework_private/app/routes/login.php
@@ -14,16 +14,37 @@ $app->post(
     '/login',
     function(Request $request, Response $response) use ($app)
     {
+        $result = SessionCheck($app);
+        if(!(is_null($result))) {
+            return $this->view->render($response,
+                'valid_login.html.twig',
+                [
+                    'css_path' => CSS_PATH,
+                    'landing_page' => LANDING_PAGE,
+                    'method' => 'post',
+                    'action' => 'displaycircutboardstate',
+                    'action2' => 'displaymessages',
+                    'page_title' => 'Login Form',
+                    'page_heading_1' => 'User logged in',
+                ]);}
+        else {
 
-        return $this->view->render($response,
-            'login.html.twig',
-            [
-                'css_path' => CSS_PATH,
-                'landing_page' => LANDING_PAGE,
-                'method' => 'post',
-                'action' => 'afterlogin',
-                'page_title' => 'Login Form',
-                'page_heading_1' => 'Login To View Content',
-            ]);
+            return $this->view->render($response,
+                'login.html.twig',
+                [
+                    'css_path' => CSS_PATH,
+                    'landing_page' => LANDING_PAGE,
+                    'method' => 'post',
+                    'action' => 'afterlogin',
+                    'page_title' => 'Login Form',
+                    'page_heading_1' => 'Login To View Content',
+                ]);}
 
-    })->setName('login');
+    })->setName('afterlogin');
+
+function SessionCheck($app)
+{
+    $session_wrapper = $app->getContainer()->get('SessionWrapper');
+    $store_var = $session_wrapper->getSessionVar('password');
+    return $store_var;
+}
