1cf975a10d95a24ad34777e2c6eaf2cc62d83912
bioreset <darek.szlag@gmail.com>
2020-01-16 17:18:16 +0000

    SettingsModel + updated db script

---
+++ ctec3110coursework_private/app/src/SettingsModel.php
@@ -0,0 +1,104 @@
+<?php
+
+
+namespace M2MAPP;
+
+
+class SettingsModel
+{
+    private $database_wrapper;
+    private $database_connection_settings;
+    private $sql_queries;
+
+    public function __construct(){}
+
+    public function __destruct(){}
+
+    public function setDatabaseWrapper($database_wrapper)
+    {
+        $this->database_wrapper = $database_wrapper;
+    }
+
+    public function setDatabaseConnectionSettings($database_connection_settings)
+    {
+        $this->database_connection_settings = $database_connection_settings;
+    }
+
+    public function setSqlQueries($sql_queries)
+    {
+        $this->sql_queries = $sql_queries;
+    }
+
+    public function makeConnectionAndQuery($query,$params=''){
+
+        $this->database_wrapper->setDatabaseConnectionSettings($this->database_connection_settings);
+
+        $this->database_wrapper->makeDatabaseConnection();
+
+        $this->database_wrapper->safeQuery($query, $params);
+
+    }
+
+    /* TODO: wsdl/wsdl_username/wsdl_password/wsdl_messagecounter/db_host/db_name/db_port/db_user/db_userpassword/db_charset/db_collation/doctrine_driver/app_name */
+
+    public function getSettingsFromDB()
+    {
+        $settings = [];
+
+        $query_string_get = $this->sql_queries->getSettings();
+
+        $this->makeConnectionAndQuery($query_string_get);
+
+        $number_of_data_sets = $this->database_wrapper->countRows();
+
+        if ($number_of_data_sets === 1) {
+            while ($row = $this->database_wrapper->safeFetchArray()) {
+                $settings['app_name'] = $row['app_name'];
+                $settings['wsdl'] = $row['wsdl'];
+                $settings['wsdl_username'] = $row['wsdl_username'];
+                $settings['wsdl_password'] = $row['wsdl_password'];
+                $settings['wsdl_messagecounter'] = $row['wsdl_messagecounter'];
+                $settings['db_host'] = $row['db_host'];
+                $settings['db_name'] = $row['db_name'];
+                $settings['db_port'] = $row['db_port'];
+                $settings['db_user'] = $row['db_user'];
+                $settings['db_userpassword'] = $row['db_userpassword'];
+                $settings['db_charset'] = $row['db_charset'];
+                $settings['db_collation'] = $row['db_collation'];
+                $settings['doctrine_driver'] = $row['doctrine_driver'];
+            }
+        } else {
+            $final_states[0] = 'Something is wrong';
+        }
+
+        $final_settings['settings'] = $settings;
+
+        return $final_settings;
+
+    }
+
+    public function updateSettings($final_settings=[]){
+
+        $query_string_update = $this->sql_queries->updateSettings();
+
+        $query_parameters =
+            array(':app_name' => $final_settings['app_name'],
+                ':wsdl' => $final_settings['wsdl'],
+                ':wsdl_username' => $final_settings['wsdl_username'],
+                ':wsdl_password' => $final_settings['wsdl_password'],
+                ':wsdl_messagecounter' => $final_settings['wsdl_messagecounter'],
+                ':db_host'  => $final_settings['db_host'],
+                ':db_name'  => $final_settings['db_name'],
+                ':db_port'  => $final_settings['db_port'],
+                ':db_user'  => $final_settings['db_user'],
+                ':db_userpassword'  => $final_settings['db_userpassword'],
+                ':db_charset'  => $final_settings['db_charset'],
+                ':db_collation'  => $final_settings['db_collation'],
+                ':doctrine_driver'  => $final_settings['doctrine_driver']);
+
+        $this->makeConnectionAndQuery($query_string_update, $query_parameters);
+
+    }
+
+
+}
--- database_script.sql
+++ database_script.sql
@@ -24,6 +24,30 @@ CREATE TABLE `messages` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app_name` varchar(128) NOT NULL,
+  `wsdl` varchar(512) NOT NULL,
+  `wsdl_username` varchar(64) NOT NULL,
+  `wsdl_password` varchar(64) NOT NULL,
+  `wsdl_messagecounter` int(11) NOT NULL,
+  `db_host` varchar(64) NOT NULL,
+  `db_name` varchar(64) NOT NULL,
+  `db_port` int(11) NOT NULL,
+  `db_user` varchar(64) NOT NULL,
+  `db_userpassword` varchar(64) NOT NULL,
+  `db_charset` varchar(64) NOT NULL,
+  `db_collation` varchar(64) NOT NULL,
+  `doctrine_driver` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `settings` (`id`, `app_name`, `wsdl`, `wsdl_username`, `wsdl_password`, `wsdl_messagecounter`, `db_host`, `db_name`, `db_port`, `db_user`, `db_userpassword`, `db_charset`, `db_collation`, `doctrine_driver`) VALUES
+(1,	'M2MAPP',	'https://m2mconnect.ee.co.uk/orange-soap/services/MessageServiceByCountry?wsdl',	'19_Sebastian',	'passXDword123',	50,	'localhost',	'coursework',	3306,	'p3tuser',	'password',	'utf8',	'utf8_unicode_ci',	'pdo_mysql');
+
+
 DROP TABLE IF EXISTS `switch`;
 CREATE TABLE `switch` (
   `id` int(4) NOT NULL DEFAULT 1,
@@ -36,8 +60,6 @@ CREATE TABLE `switch` (
   `keypad` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `switch` (`id`, `switch1`, `switch2`, `switch3`, `switch4`, `fan`, `heater`, `keypad`) VALUES
-(1,	'off',	'on',	'off',	'on',	'forward',	'11',	'7');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -56,5 +78,3 @@ CREATE TABLE `user_data` (
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`auto_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
